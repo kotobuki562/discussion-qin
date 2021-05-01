05e245491c327424086897987fb796778cff65e2
MaQueen <kotobukitsukasaneko33@docomo.com>
2021-05-01 22:01:48 +0900

    🍴

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,9 @@
 import { useState, useEffect } from "react";
 import { supabase } from "../util/supabase";
-import { format, formatISO } from "date-fns";
 import { Layout } from "../components/Layout";
 
 const Home = () => {
-  const [name, setName] = useState("");
-  const [title, setTitle] = useState("");
-  const [text, setText] = useState("");
   const [posts, setPosts] = useState<any[]>([]);
-  const today = formatISO(new Date());
-
-  const createPost = async () => {
-    try {
-      await supabase.from("posts").insert([
-        {
-          name: name,
-          title: title,
-          text: text,
-          createAt: today,
-          updateAt: today,
-          check: false,
-        },
-      ]);
-    } catch (error) {
-      console.log(error);
-    }
-  };
 
   const fetchPost = () => {
     const postRef = supabase.from("posts").select("*");
@@ -44,36 +22,6 @@ const Home = () => {
   return (
     <Layout>
       <div>
-        <div className="bg-red-200 flex flex-col items-center">
-          <input
-            className="border-2"
-            type="text"
-            name="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <input
-            className="border-2"
-            type="text"
-            name="title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-          <input
-            className="border-2"
-            type="text"
-            name="text"
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-          />
-          <button
-            onClick={() => {
-              createPost();
-            }}
-          >
-            送信
-          </button>
-        </div>
         <div className="bg-blue-200 flex flex-col items-center">
           {posts.map((post) => {
             return (
--- src/pages/post/create.tsx
+++ src/pages/post/create.tsx
@@ -1,7 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+import { supabase } from "../../util/supabase";
+import { format, formatISO } from "date-fns";
+import { InputForm } from "../../components/Input/Input";
+import { Layout } from "../../components/Layout";
 
 const CreatePost = () => {
-  return <div></div>;
+  const [name, setName] = useState("");
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
+  const today = formatISO(new Date());
+
+  const inputValues = [
+    {
+      type: "text",
+      name: "name",
+      value: name,
+      onChange: (e) => setName(e.target.value),
+      placeholder: "お名前は？🤔",
+    },
+    {
+      type: "text",
+      name: "title",
+      value: title,
+      onChange: (e) => setTitle(e.target.value),
+      placeholder: "お題タイトル",
+    },
+    {
+      textArea: true,
+      type: "text",
+      name: "text",
+      value: text,
+      onChange: (e) => setText(e.target.value),
+      placeholder: "お題の詳細文",
+    },
+  ];
+
+  const createPost = async () => {
+    try {
+      await supabase.from("posts").insert([
+        {
+          name: name,
+          title: title,
+          text: text,
+          createAt: today,
+          updateAt: today,
+          check: false,
+        },
+      ]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <Layout>
+      <div className="bg-red-200 flex flex-col items-center">
+        <InputForm inputs={inputValues} />
+        <button
+          onClick={() => {
+            createPost();
+          }}
+        >
+          送信
+        </button>
+      </div>
+    </Layout>
+  );
 };
 
 export default CreatePost;
